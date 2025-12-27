a2eb3d33122ddbbca70f7874404fb675f28d11e3
My-practice-repository <asmaasma3528@gmail.com>
2025-12-27 05:34:18 +0530

    Added the logic in the home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,6 +3,9 @@ import styles from "./HomePage.module.css";
 import ProductList from "../../components/Product/ProductList/ProductList";
 import FilterSidebar from "../../components/FilterSidebar/FilterSidebar";
 
+import { fetchProducts, getVisibleItems, getLoading, getFilters } from "../../redux/reducers/productsReducer";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../../components/UI/Loader/Loader";
 
 function HomePage() {
   const [query, setQuery] = useState("");
@@ -14,12 +17,27 @@ function HomePage() {
     womensClothing: false,
   });
 
+  const dispatch = useDispatch();
+//redux selectors
+const products = useSelector(getVisibleItems);
+const loading = useSelector(getLoading);
+
   // Fetch products on app mount
-
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [dispatch])
   // Rerender the products if the search or filter parameters change
-
+  useEffect(() => {
+    dispatch(getFilters({
+      searchText:query,
+      maxPrice:priceRange,
+      selectedCategory:categories
+    }))
+  }, [query, priceRange, categories, products.length, dispatch]);
   // Display loader while products are fetching using the Loader Component
-
+  if(loading){
+    return <Loader />
+  }
   return (
     <div className={styles.homePageContainer}>
       <FilterSidebar
